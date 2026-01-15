2c721168d60ae4f8fe4a0755d038ccd0c3a431e7
Kevin Blalock <kevin.blalock@gmail.com>
2026-01-15 17:30:00 -0600

    feat(02-02): export ProxmoxCollector from collectors package
    - Add ProxmoxCollector to package exports
    - Available via `from netbox_auto.collectors import ProxmoxCollector`

--- src/netbox_auto/collectors/__init__.py
+++ src/netbox_auto/collectors/__init__.py
@@ -5,5 +5,7 @@ Provides collector implementations for various host discovery sources.
 
 from netbox_auto.collectors.base import Collector, DiscoveredHost
 from netbox_auto.collectors.dhcp import DHCPCollector
+from netbox_auto.collectors.proxmox import ProxmoxCollector
+from netbox_auto.collectors.scanner import ScannerCollector
 
-__all__ = ["Collector", "DiscoveredHost", "DHCPCollector"]
+__all__ = ["Collector", "DiscoveredHost", "DHCPCollector", "ProxmoxCollector", "ScannerCollector"]
